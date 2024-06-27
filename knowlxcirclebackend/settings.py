04f8f32c26c313d9e79374a95a9aac1c4545dc04
Thirza <thirzahmad@gmail.com>
2024-06-27 23:09:45 +0700

    init

--- knowlxcirclebackend/settings.py
+++ knowlxcirclebackend/settings.py
@@ -13,22 +13,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
+import firebase_admin
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ezlrnptzz18-hd%-2)0%ut_@@avy=oruecnbsjoc)@p(euhun="
+SECRET_KEY = os.environ.get("DJANGO_API_KEY")
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+app = firebase_admin.initialize_app()
 
 # Application definition
 
