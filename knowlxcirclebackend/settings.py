10c8d59392fd855410ccdd0613845cf295fe9f48
Thirza <thirzahmad@gmail.com>
2024-07-08 13:01:39 +0700

    fixs

--- knowlxcirclebackend/settings.py
+++ knowlxcirclebackend/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_API_KEY")
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+SECRET_KEY = os.environ["DJANGO_API_KEY"]
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "https://knowlxcircleapi.azurewebsites.net/"]
 
 app = firebase_admin.initialize_app()
 
