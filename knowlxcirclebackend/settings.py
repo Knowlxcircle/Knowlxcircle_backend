837f8be2e1c9061b85ecc5469d501f1a95b327be
Thirza <thirzahmad@gmail.com>
2024-07-10 23:53:27 +0700

    add cors

--- knowlxcirclebackend/settings.py
+++ knowlxcirclebackend/settings.py
@@ -187,3 +187,12 @@ CACHES = {
         },
     }
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://knowlxcircleapi.azurewebsites.net',
+    'https://knowlxcirclebackend-production.up.railway.app'
+]
